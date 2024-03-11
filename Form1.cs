36e582c921998ce4565e28e58dece1a6df3efd2d
2003474 <2003474@apps.nsd.org>
2024-03-11 09:55:30 -0700

    Fixed Json bug

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Text.Json.Serialization;
 using System.Windows.Forms;
 //using Newtonsoft.Json;
 using RestSharp;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace WindowsFormsApp1
 {
@@ -71,19 +72,57 @@ namespace WindowsFormsApp1
             request.AddHeader("Authorization", String.Format("Basic {0}", basicAuthValue));
 
             response = await client.GetAsync(request);
-            Console.WriteLine(response.Content);
             x = Newtonsoft.Json.JsonConvert.DeserializeObject(response.Content);
             result = x.result;
             var tagsPlaceholder = result.tags;
             Tag[] tags = new Tag[10];
-            for(int i = 0; i < tags.Length; i++)
+
+           
+            for (int i = 0; i < tags.Length; i++)
             {
                 Console.Out.WriteLine();
-                tags[i].confidence = tagsPlaceholder[i].confidence.ToObject<decimal>();
-                tags[i].confidence = tagsPlaceholder[i].tag.en.ToString();
+                tags[i] = new Tag
+                {
+                    confidence = tagsPlaceholder[i].confidence,
+                    name = tagsPlaceholder[i].tag.en,
+                };
             }
 
+            textBox1.Visible = true;
+            textBox2.Visible = true;
+            textBox3.Visible = true;
+            textBox4.Visible = true;
+            textBox5.Visible = true;
+            textBox6.Visible = true;
+            textBox7.Visible = true;
+            textBox8.Visible = true;
+            textBox9.Visible = true;
+            textBox10.Visible = true;
 
+            progressBar1.Visible = true;
+            progressBar2.Visible = true;
+            progressBar3.Visible = true;
+            progressBar4.Visible = true;
+            progressBar5.Visible = true;
+
+            textBox1.Text = tags[0].name + ":";
+            textBox2.Text = tags[1].name + ":";
+            textBox3.Text = tags[2].name + ":";
+            textBox4.Text = tags[3].name + ":";
+            textBox5.Text = tags[4].name + ":";
+
+
+            progressBar1.Value = (int)tags[0].confidence;
+            progressBar2.Value = (int)tags[1].confidence;
+            progressBar3.Value = (int)tags[2].confidence;
+            progressBar4.Value = (int)tags[3].confidence;
+            progressBar5.Value = (int)tags[4].confidence;
+
+            textBox10.Text = (int)tags[0].confidence + "%";
+            textBox9.Text = (int)tags[1].confidence + "%";
+            textBox8.Text = (int)tags[2].confidence + "%";
+            textBox7.Text = (int)tags[3].confidence + "%";
+            textBox6.Text = (int)tags[4].confidence + "%";
 
 
 
