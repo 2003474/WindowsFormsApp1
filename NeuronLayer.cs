964c60c40ed981d671b72e7b1236e38e34048fcb
2003474 <106296420+2003474@users.noreply.github.com>
2024-03-11 12:36:42 -0700

    added output method to class
    returns an array of outputs from the neurons

--- NeuronLayer.cs
+++ NeuronLayer.cs
@@ -10,5 +10,13 @@ namespace WindowsFormsApp1
     {
         decimal[] inputs;
         Neuron[] neurons;
+    
+        public decimal[] output() {
+            decimal[] output = new decimal[neurons.size()];
+            for(int i = 0; i < output.size(); i++) {
+                output[i] = neurons[i].output(inputs);
+            }
+            return output;
+        }
     }
 }
