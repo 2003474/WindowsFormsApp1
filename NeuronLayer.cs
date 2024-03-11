717fe8742b6439a4b5df00591d773421cf868c1a
2003474 <106296420+2003474@users.noreply.github.com>
2024-03-11 12:29:25 -0700

    Created NeuronLayer class

--- NeuronLayer.cs
+++ NeuronLayer.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace WindowsFormsApp1
+{
+    internal class NeuronLayer
+    {
+        decimal[] inputs;
+        Neuron[] neurons;
+    }
+}
