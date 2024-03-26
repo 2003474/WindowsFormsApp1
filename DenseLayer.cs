fae025db877c2e84ed841423e9036fbeed1f4706
2003474 <106296420+2003474@users.noreply.github.com>
2024-03-26 12:57:53 -0700

    Dense Layer mergine
    needs testing but prolly works

--- DenseLayer.cs
+++ DenseLayer.cs
@@ -16,6 +16,19 @@
         public DenseLayer(DenseLayer layer1, DenseLayer layer2, int mutationLvl)
         {
             // for every neuron
+            this.neurons = new DenseNeuron[num_neurons];
+            Random rnd = new Random();
+            int num = rnd.Next();
+            for (int i = 0; i < num_neurons; i++)
+            {    num = rnd.Next(1, 4);
+                 if(num == 1) {
+                     neurons[i] = layer1.neurons[i];
+                 } else if ( num == 2) {
+                     neurons[i] = layer2.neurons[i];
+                 } else {
+                     neurons[i] = new DenseNeuron(layer1.neurons[i], layer2.neurons[i]);
+                 }
+            }
             // randomly chooses between neurons either a 0 1 2
             // 0 is layer1 neruon
             // 1 is layer2 neuron
