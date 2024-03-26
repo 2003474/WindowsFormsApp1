249a97e75c03543b8cc1b4043d3f4f926f086844
2003474 <106296420+2003474@users.noreply.github.com>
2024-03-26 13:10:40 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,8 +12,10 @@ namespace WindowsFormsApp1
         {
             NetworkTrainer w = new NetworkTrainer(1, 2, 8, 10);
             NeuralNetwork final = w.Train();
-            final.input = new double[1] { 25.4 };
+            
+            final.input = new double[1] { 25.0 };
             final.Forward();
+            // should return 0 and 1 or extremely close, like EXTREMELY close
             Console.WriteLine(final.output[0] + ", " + final.output[1]);
             //NeuralNetwork network = new NeuralNetwork(2, 2, 8);
             //network.input = input;
