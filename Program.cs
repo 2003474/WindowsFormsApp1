5f2a2c9c37da3e6cff01f5d9df82cdbc4ff911b0
2003474 <106296420+2003474@users.noreply.github.com>
2024-03-26 13:08:59 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApp1
         {
             NetworkTrainer w = new NetworkTrainer(1, 2, 8, 10);
             NeuralNetwork final = w.Train();
+            final.input = new double[1] { 25.4 };
+            final.Forward();
+            Console.WriteLine(final.output[0] + ", " + final.output[1]);
             //NeuralNetwork network = new NeuralNetwork(2, 2, 8);
             //network.input = input;
             //network.Forward();
